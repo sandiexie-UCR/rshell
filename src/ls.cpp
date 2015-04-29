a008e2b2b0ba521e142471122e22c1c2ece6e8bf
zxie003 <zxie003@ucr.edu>
2015-04-29 02:30:41 -0700

    worked on -a and -l

--- src/ls.cpp
+++ src/ls.cpp
@@ -3,6 +3,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pwd.h>
+#include <cstring>
+#include <dirent.h>
+#include <sys/types.h>
+#include <algorithm>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 using namespace std;
 
@@ -51,6 +61,144 @@ void get_directory (bool& a, bool& l, bool& R, bool& g, vector<string>& d, char*
 	}
 }
 
+bool comparedir (string a, string b)
+{
+	return a.compare(b) <0;
+}
+
+void sort_dir (vector<string>& f)
+{
+	sort(f.begin(), f.end(), comparedir);
+}
+
+void print_file (bool& do_all, bool& do_l, vector<string> filenames, string dir)
+{
+	if (!do_all)
+	{
+		//fix vector is it is not a
+		while( filenames.at(0).at(0) == '.' )
+		{
+			filenames.erase(filenames.begin());
+		}
+	}
+	
+	if (! do_l )
+	{
+		// just print names
+		for (unsigned int i =0; i < filenames.size();i++)
+		{
+			char *width = (char*)malloc(BUFSIZ);
+
+			strcat (width,(char*) filenames.at(i).c_str());
+
+			cout << filenames.at(i) << "  ";
+		}
+	}
+	else
+	{
+		cout << "here"<< endl;
+		//do l
+		//print infos
+		for (unsigned int i=0; i<filenames.size();i++)
+		{
+			string path = dir + '/' + filenames.at(i);
+
+			cout << "::" << endl;
+			struct stat f1;
+			stat(path.c_str(), &f1);
+			cout << "!!";	
+			// access part:
+			if (S_ISDIR(f1.st_mode))
+			{
+				cout << "rrrr";
+				cout << 'd';
+			}
+			else if (S_ISLNK(f1.st_mode))
+			{
+				cout << 's';
+			}
+			else
+			{
+				cout << '-';
+			}
+
+			//check for each one:
+			if (f1.st_mode & S_IRUSR)
+				cout << 'r';
+			else
+				cout << '-';
+			
+			if (f1.st_mode & S_IWUSR)
+				cout << 'w';
+			else
+				cout << '-';
+
+			if (f1.st_mode & S_IXUSR)
+				cout << 'x';
+			else
+				cout << '-';
+
+			if (f1.st_mode & S_IRGRP)
+				cout << 'r';
+			else
+				cout << '-';
+			
+			if (f1.st_mode & S_IWGRP)
+				cout << 'w';
+			else
+				cout << '-';
+
+			if (f1.st_mode & S_IXGRP)
+				cout << 'x';
+			else
+				cout << '-';
+
+			if (f1.st_mode & S_IROTH)
+				cout << 'r';
+			else
+				cout << '-';
+			
+			if (f1.st_mode & S_IWOTH)
+				cout << 'w';
+			else
+				cout << '-';
+
+			if (f1.st_mode & S_IXOTH)
+				cout << 'x';
+			else
+				cout << '-';
+
+			cout << " ";
+
+			struct passwd* name;
+			struct group* group;
+
+			string name_s;
+			string group_s;
+
+			string new1 = dir + '/' + filenames.at(i);
+			
+			name = getpwuid(f1.st_uid);
+
+			group = getgrgid(f1.st_gid);
+			
+			name_s = name->pw_name;
+			group_s = group->gr_name;
+
+			string time;
+			time = ctime(&f1.st_mtime);
+			time = time.substr(4,12);
+
+			cout.width(2); cout << right << f1.st_nlink;
+			cout << " " << name_s;
+			cout << " " << group_s;
+			cout << " " << f1.st_size << " " << time << " ";
+
+		}
+	}
+
+}
+
 int main (int argc, char * argv[])
 {
 	bool has_a = false;
@@ -71,7 +219,50 @@ int main (int argc, char * argv[])
 	cout << "a: " << has_a << endl;
 	cout << "l: " << has_l << endl;
 	cout << "g: " << has_g << endl;
-	
-	cout << directory.at(0);
+	cout << "R: " << has_R << endl;	
+	//cout << directory.at(0)<< endl;
+	//cout << directory.at(1);
+
+	for (int i = 0; i < directory.size(); i++)
+	{
+		// loop for each directory
+
+		vector <string> files;
+			
+		DIR *opened_dir = opendir((char* ) directory.at(i).c_str());
+		
+		cout << "222" << endl;
+
+		if (opened_dir == NULL)
+		{
+			perror("open not existing directory.");
+			exit(1);
+		}
+
+		dirent *a;
+		while ( (a = readdir(opened_dir)) )
+		{
+			if (a == NULL)
+			{
+				perror ("read directory fail");
+				exit(1);
+			}
+
+			string b = a-> d_name;
+			files.push_back (b);
+		}
+
+		sort_dir (files);
+
+		// finish getting files from directory----------------------------------------
+
+
+		for (int y =0; y<files.size();y++)
+		{
+			cout << files.at(y)<< endl;
+		}
+
+		print_file (has_a, has_l, files, directory.at(i));
+	}
 	return 0;
 }
